2b89b3a77b08fc9ffa422e73b792725af9bdd2b4
Steven Yeu <5847873+StevenYeu@users.noreply.github.com>
2023-06-16 10:44:56 -0700

    Update packer.lua

--- lua/syeu/packer.lua
+++ lua/syeu/packer.lua
@@ -27,11 +27,16 @@ return require('packer').startup(function(use)
   use("tpope/vim-fugitive")
   use {
 	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
+	  branch = 'v2.x',
 	  requires = {
 		  -- LSP Support
 		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
+		  {                                      -- Optional
+		  'williamboman/mason.nvim',
+			  run = function()
+			      pcall(vim.cmd, 'MasonUpdate')
+			  end,
+		  },
 		  {'williamboman/mason-lspconfig.nvim'},
 
 		  -- Autocompletion
