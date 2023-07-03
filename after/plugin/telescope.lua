9cbf807c648bb125d1fb808dbf8f7a0109b92131
Steven Yeu <5847873+StevenYeu@users.noreply.github.com>
2023-07-02 23:31:01 -0700

    Update telescope.lua

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,17 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fw', function()
+local builtin = require("telescope.builtin")
+local wk = require("which-key")
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
+vim.keymap.set("n", "<leader>fw", function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+wk.register({
+	f = {
+		name = "Fuzzy Finder",
+		f = { "Find Files" },
+		g = "Search  File",
+		w = "Grep Search",
+	},
+}, { prefix = "<leader>" })
