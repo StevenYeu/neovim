85d9e628143d818979de90bbb796c2a8f0c76e83
Steven Yeu <5847873+StevenYeu@users.noreply.github.com>
2023-07-02 23:25:01 -0700

    added whichkey desc for barbar

--- after/plugin/barbar.lua
+++ after/plugin/barbar.lua
@@ -1,3 +1,11 @@
+local wk = require("which-key")
+
 vim.keymap.set("n", "<tab>", "<Cmd>BufferNext<CR>")
-vim.keymap.set("n", "<leader>x", "<Cmd>BufferClose<CR>")
+vim.keymap.set("n", "<leader>w", "<Cmd>BufferClose<CR>")
 vim.keymap.set("n", "<S-tab>", "<Cmd>BufferPrevious<CR>")
+
+wk.register({
+	["<leader>w"] = { "Close Buffer Window" },
+	["<tab>"] = { "Next Buffer Window" },
+	["<S-tab>"] = { "Previous Buffer Window" },
+})
