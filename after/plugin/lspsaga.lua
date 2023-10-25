0fab2826836135d8ff7664319e18a7f92c8c009a
Steven Yeu <syeu@ucsd.edu>
2023-10-24 17:23:55 -0700

    remap keys for lsp

--- after/plugin/lspsaga.lua
+++ after/plugin/lspsaga.lua
@@ -1,105 +1,65 @@
 local wk = require("which-key")
 local keymap = vim.keymap.set
 
--- LSP finder - Find the symbol's definition
--- If there is no definition, it will instead be hidden
--- When you use an action in finder like "open vsplit",
--- you can use <C-t> to jump back
-keymap("n", "gh", "<cmd>Lspsaga finder<CR>")
+keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { desc = "Code Action" })
 
--- Code action
-keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+keymap("n", "vrf", "<cmd>Lspsaga rename<CR>", { desc = "Rename symbol in file" })
+keymap("n", "vrp", "<cmd>Lspsaga rename ++project<CR>", { desc = "Rename symbol in project" })
 
--- Rename all occurrences of the hovered word for the entire file
-keymap("n", "vgr", "<cmd>Lspsaga rename<CR>")
-
--- Rename all occurrences of the hovered word for the selected files
---keymap("n", "grp", "<cmd>Lspsaga rename ++project<CR>")
-
--- Peek definition
 -- Use <C-t> to jump back
-keymap("n", "gp", "<cmd>Lspsaga peek_definition<CR>")
+keymap("n", "gp", "<cmd>Lspsaga peek_definition<CR>", { desc = "Peek Definition" })
+keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>", { desc = "Goto Definition" })
+keymap("n", "gt", "<cmd>Lspsaga goto_type_definition<CR>", { desc = "Goto Type Definition" })
+keymap("n", "gh", "<cmd>Lspsaga finder<CR>", { desc = "Find Sysbol Definition" })
 
--- Go to definition
-keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
+keymap(
+    "n",
+    "<leader>sl",
+    "<cmd>Lspsaga show_line_diagnostics<CR>",
+    { desc = "Show Line Diagnostic" }
+)
 
--- Peek type definition
--- Use <C-t> to jump back
---keymap("n", "gt", "<cmd>Lspsaga peek_type_definition<CR>")
+keymap(
+    "n",
+    "<leader>sb",
+    "<cmd>Lspsaga show_buf_diagnostics<CR>",
+    { desc = "Show Buffer Diagnostic" }
+)
 
--- Go to type definition
-keymap("n", "gt", "<cmd>Lspsaga goto_type_definition<CR>")
+keymap(
+    "n",
+    "<leader>sw",
+    "<cmd>Lspsaga show_workspace_diagnostics<CR>",
+    { desc = "Show Workspace Diagnostic" }
+)
 
--- Show line diagnostics
--- You can pass argument ++unfocus to
--- unfocus the show_line_diagnostics floating window
-keymap("n", "<leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>")
-
--- Show buffer diagnostics
-keymap("n", "<leader>sb", "<cmd>Lspsaga show_buf_diagnostics<CR>")
-
--- Show workspace diagnostics
-keymap("n", "<leader>sw", "<cmd>Lspsaga show_workspace_diagnostics<CR>")
-
--- Show cursor diagnostics
-keymap("n", "<leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
+keymap(
+    "n",
+    "<leader>sc",
+    "<cmd>Lspsaga show_cursor_diagnostics<CR>",
+    { desc = "Show Cursor Diagnostic" }
+)
 
 -- Diagnostic jump
 -- You can use <C-o> to jump back to your previous location
-keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
-keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
-
--- Diagnostic jump with filters such as only jumping to an error
+keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { desc = "Jump Prev Diagnostic" })
+keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { desc = "Jump Next Diagnostic" })
 keymap("n", "[E", function()
-	require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
-end)
+    require("lspsaga.diagnostic"):goto_prev({
+        severity = vim.diagnostic.severity.ERROR,
+    })
+end, { desc = "Jump Prev Error Diagnostic" })
 keymap("n", "]E", function()
-	require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
-end)
+    require("lspsaga.diagnostic"):goto_next({
+        severity = vim.diagnostic.severity.ERROR,
+    })
+end, { desc = "Jump Next Error Diagnostic" })
 
--- Toggle outline
-keymap("n", "<leader>o", "<cmd>Lspsaga outline<CR>")
+keymap("n", "<leader>o", "<cmd>Lspsaga outline<CR>", { desc = "Toogle Outline" })
 
--- Hover Doc
 -- To disable it just use ":Lspsaga hover_doc ++quiet"
 -- Pressing the key twice will enter the hover window
-keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>")
+keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", { desc = "Hover" })
 
--- If you want to keep the hover window in the top right hand corner,
--- you can pass the ++keep argument
--- Note that if you use hover with ++keep, pressing this key again will
--- close the hover window. If you want to jump to the hover window
--- you should use the wincmd command "<C-w>w"
---keymap("n", "K", "<cmd>Lspsaga hover_doc ++keep<CR>")
-
--- Call hierarchy
-keymap("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
-keymap("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
-
--- Floating terminal
-keymap({ "n", "t" }, "<A-d>", "<cmd>Lspsaga term_toggle<CR>")
-
-keymap("n", "vrf", "<cmd>Lspsaga finder ref <CR>")
-
-wk.register({
-	["<leader>gh"] = { "Find Symbol Def" },
-	["<leader>ca"] = { "Code Action" },
-	["vgr"] = { "Rename All Occurances" },
-	["gp"] = { "Peek Definition" },
-	["gd"] = { "Go to Definition" },
-	["gt"] = { "Go to Definition" },
-	["<leader>sl"] = { "Show Line Diagnostic" },
-	["<leader>sb"] = { "Show Buffer Diagnostic" },
-	["<leader>sw"] = { "Show Worspace Diagnostic" },
-	["<leader>sc"] = { "Show Cursor Diagnostic" },
-	["[e"] = { "Jump Prev Diagnostic" },
-	["]e"] = { "Jump Next Diagnostic" },
-	["[E"] = { "Jump Prev Error Diagnostic" },
-	["]E"] = { "Jump Next Error Diagnostic" },
-	["<leader>o"] = { "Toogle Outline" },
-	["K"] = { "Hover" },
-	["<leader>cb"] = { "Show outgoing calls" },
-	["<leader>ci"] = { "Show incoming calls" },
-	["vrf"] = { "Show References, Implementations, Definitions" },
-	["<leader>fc"] = { "Format Code" },
-}, { mode = "n" })
+keymap("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>", { desc = "Show incoming calls" })
+keymap("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>", { desc = "Show outgoing calls" })
--- lua/syeu/remap.lua
+++ lua/syeu/remap.lua
@@ -1,25 +1,30 @@
 local wk = require("which-key")
 
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "File Viewer" })
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Highlighted Line Down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move Highlighted Line Up" })
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Appends line below to end of current line" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Page Down Nav" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Page Up Nav" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Keeps search term in middle" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Keeps search term in middle" })
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste and flush buffer" })
 
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy in system clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Copy in system clipboard" })
 
 vim.keymap.set("n", "Q", "<nop>")
 
-vim.keymap.set("n", "<leader>rs", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set(
+	"n",
+	"<leader>rs",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "Replace highlighted word" }
+)
 
 -- Quick Fix
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
@@ -27,39 +32,7 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>q", "^")
-vim.keymap.set("n", "<leader>e", "$")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", { desc = "Switch Tmux session" })
+vim.keymap.set("n", "<leader>q", "^", { desc = "Go to start of line" })
+vim.keymap.set("n", "<leader>e", "$", { desc = "Go to end of line" })
 
-wk.register({
-	["<leader>q"] = { "Go to start of line" },
-	["<leader>e"] = { "Go to end of line" },
-}, { mode = "n" })
-wk.register({
-	["<leader>pv"] = { "File Viewer" },
-	["J"] = { "Move Highlighted Line Down" },
-	["K"] = { "Move Highligted Line Up" },
-}, { mode = "v" })
-
-wk.register({
-	["J"] = { " Appends line below to end of current line" },
-	["<C-d>"] = { "Page Down Nav" },
-	["<C-u<"] = { "Page Up Nav" },
-	["n"] = { "Keeps Serach Term in Middle" },
-	["N"] = { "Keeps Search Term in Middle" },
-	["<leader>rs"] = { "Replace Highligted Word with new word" },
-	["<C-f>"] = { "Switch Tmux Session" },
-}, { mode = "n" })
-
-wk.register({
-	["<leader>p"] = { "Paste and flush buffer" },
-}, { mode = "x" })
-
-wk.register({
-	y = { "Copy in system clipboard" },
-	d = { "Delete and flush buffer" },
-}, { mode = { "n", "v" }, prefix = "<leader>" })
-
-wk.register({
-	y = { "Copy in system clipboard" },
-}, { mode = { "n", "v" }, prefix = "<leader>" })
