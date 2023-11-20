ac4dcb913c3b83486dd13da1f5c7f98c3da9d0df
Steven Yeu <stevenhanhyeu@gmail.com>
2023-11-20 13:38:44 -0800

    added keybids for hidden file search and live grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,15 +3,21 @@ local wk = require("which-key")
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
+vim.keymap.set("n", "<leader>fl", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fw", function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set("n", "<leader>fh", function()
+    builtin.find_files({ find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" } })
 end)
 
 wk.register({
-	f = {
-		name = "Fuzzy Finder",
-		f = { "Find Files" },
-		g = "Search  File",
-		w = "Grep Search",
-	},
+    f = {
+        name = "Fuzzy Finder",
+        f = { "Find Files" },
+        g = "Search Git Files",
+        w = "Grep Search",
+        l = "Live Grep Search",
+        h = "Search Hidden Files"
+    },
 }, { prefix = "<leader>" })
