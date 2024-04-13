a462f5604c5c8bdbd7353a83ed68a477fcc2f38e
Steven Yeu <syeu@sdsc.edu>
2024-04-13 11:11:06 -0700

    changed keymap for lsp functions

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -9,14 +9,14 @@ lsp_zero.on_attach(function(_, bufnr)
         if desc then
             desc = "LSP: " .. desc
         end
-        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc})
     end
     nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
     nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
     nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
     nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-    nmap("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+    nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
     nmap("gl", vim.diagnostic.open_float, "[S]how [D]iagnostic")
     nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
     nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
