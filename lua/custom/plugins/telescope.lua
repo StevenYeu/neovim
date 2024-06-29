cd3d59203ca6f85b25623fbf46110dc5e8c36435
Steven Yeu <stevenhanhyeu@gmail.com>
2024-06-29 13:53:06 -0700

    Updated telescope find settings

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -13,19 +13,36 @@ return {
     },
     config = function()
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>ff",
-            function() builtin.find_files() end,
-            { desc = "Find Files" })
+        -- vim.keymap.set("n", "<leader>ff", function()
+        --     builtin.find_files()
+        -- end, { desc = "Find Files" })
         vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Search Git Files" })
-        vim.keymap.set("n", "<leader>fl", function() builtin.live_grep({ file_ignore_patterns = { "vendor" } }) end,
-            { desc = "Grep Search" })
+        vim.keymap.set("n", "<leader>fl", function()
+            builtin.live_grep({ file_ignore_patterns = { "vendor", "node_modules" } })
+        end, { desc = "Grep Search" })
         vim.keymap.set("n", "<leader>fw", function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end, { desc = "Live Grep Search" })
+        vim.keymap.set("n", "<leader>ff", function()
+            builtin.find_files({
+                find_command = {
+                    "rg",
+                    "--files",
+                    "--hidden",
+                    "-g",
+                    "!**/.git/*",
+                    "-g",
+                    "!**/vendor/**",
+                    "-g",
+                    "!**/node_modules/**",
+                },
+            })
+        end, { desc = "Find File" })
+
         vim.keymap.set("n", "<leader>fd", function()
-            builtin.find_files({ find_command = { "rg", "--files", "--hidden", "-g", "!**/.git/*", "-g", "!**/vendor/**" } })
-        end, { desc = "Find File RG" })
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Search Help" })
-        vim.keymap.set('n', '<leader>fj', builtin.jumplist, { desc = "Search JumpList" })
-    end
+            builtin.find_files({ hidden = true, no_ignore = true })
+        end, { desc = "Find File Hidden" })
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Search Help" })
+        vim.keymap.set("n", "<leader>fj", builtin.jumplist, { desc = "Search JumpList" })
+    end,
 }
