f419f1cb69e8539babd3e705647882cc1df867d3
Steven Yeu <stevenhanhyeu@gmail.com>
2024-12-25 01:29:09 -0800

    update rose pine config

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -34,9 +34,11 @@ require("lazy").setup({
 
     {
         "rose-pine/neovim",
-        as = "rose-pine",
+        name = "rose-pine",
         config = function()
             require("rose-pine").setup({
+                variant = "main",
+                dark_variant = "main",
                 styles = {
                     transparency = true
                 }
