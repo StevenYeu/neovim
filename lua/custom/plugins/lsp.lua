c70bf96f346edd6871f285a3163375f97b961d0c
Steven Yeu <stevenhanhyeu@gmail.com>
2024-12-28 13:49:00 -0800

    update mappings for lsp

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -31,7 +31,7 @@ return {
                 },
                 window = {
                     completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
+                    -- documentation = cmp.config.window.bordered(),
                 },
                 snippet = {
                     expand = function(args)
@@ -46,10 +46,21 @@ return {
                     ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
                     -- trigger completion menu
                     ['<C-c>'] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
 
                     -- scroll up and down the documentation window
                     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-d>'] = cmp.mapping.scroll_docs(4),
+
+                    ['<C-k>'] = cmp.mapping(function(fallback)
+                        if cmp.visible_docs() then
+                            cmp.close_docs()
+                        elseif cmp.visible() then
+                            cmp.open_docs()
+                        else
+                            fallback()
+                        end
+                    end),
 
                     -- jump to the next snippet placeholder
                     ['<C-f>'] = cmp.mapping(function(fallback)
@@ -112,28 +123,28 @@ return {
             vim.api.nvim_create_autocmd('LspAttach', {
                 desc = 'LSP actions',
                 callback = function(event)
-                    local nmap = function(keys, func, desc)
+                    local nmap = function(mode, keys, func, desc)
                         if desc then
                             desc = "LSP: " .. desc
                         end
-                        vim.keymap.set("n", keys, func, { buffer = event.buf, desc = desc })
+                        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = desc })
                     end
-                    nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-                    nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+                    nmap("n", "<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+                    nmap("n", "<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-                    nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-                    nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-                    nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-                    nmap("gl", vim.diagnostic.open_float, "[S]how [D]iagnostic")
-                    nmap("gD", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-                    nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-                    nmap(
+                    nmap("n", "gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+                    nmap("n", "gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+                    nmap("n", "gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+                    nmap("n", "gl", vim.diagnostic.open_float, "[S]how [D]iagnostic")
+                    nmap("n", "gD", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+                    nmap("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+                    nmap("n",
                         "<leader>ws",
                         require("telescope.builtin").lsp_dynamic_workspace_symbols,
                         "[W]orkspace [S]ymbols"
                     )
-                    nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-                    nmap("gs", vim.lsp.buf.signature_help, "Signature Documentation")
+                    nmap("n", "K", vim.lsp.buf.hover, "Hover Documentation")
+                    nmap("i", "<C-h>", vim.lsp.buf.signature_help, "Signature Documentation")
                 end,
             })
             require("mason-lspconfig").setup({
